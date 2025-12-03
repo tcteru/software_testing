113eed810fda49baf13c4e18aad052588462f535
Alllleksi <aleksi_kehusmaa@outlook.com>
2025-12-03 09:41:20 +0200

    Added test files, and implemented one test for the add.js.

---
+++ test/add.test.js
@@ -0,0 +1,9 @@
+import {expect} from "chai";
+import add from "../src/add.js";
+
+// Tämä on testi että toimii
+describe("addTest", () => {
+    it("should return 3 when 1 added to 2",()=>{
+        expect(add(2,1)).to.equal(3);
+    });
+});
