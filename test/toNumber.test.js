f197b9c14bd69c5cbdcbe9656afe5b8edfde34ae
Aleksi Kehusmaa <aleksi.kehusmaa@tuni.fi>
2025-12-05 18:42:45 +0200

    Statement and branch coverage increased.

--- test/toNumber.test.js
+++ test/toNumber.test.js
@@ -1,6 +1,20 @@
 import toNumber from "../src/toNumber.js";
+import divide from "../src/divide.js";
 
 describe("toNumber", () => {
+
+  test("value is number", () => {
+    expect(toNumber(42)).toBe(42);
+  });
+
+  test("parameter is symbol", () => {
+    expect(Number.isNan("a")).toBe(true);
+  });
+
+  test("parameter is function", () => {
+    expect(divide(2,2)).toBe(1);
+  });
+
   test("converts numeric string", () => {
     expect(toNumber("42")).toBe(42);
   });
@@ -15,8 +29,10 @@ describe("toNumber", () => {
     expect(Number.isNaN(toNumber(undefined))).toBe(true);
   });
 
-  test("hex and scientific notation if supported", () => {
+  test("hex, binary and scientific notation if supported", () => {
+    expect(toNumber("0xf")).toBe(15);
     expect(toNumber("0x10")).toBe(16);
+    expect(toNumber("0b1011")).toBe(11);
     expect(toNumber("1e3")).toBe(1000);
   });
 
